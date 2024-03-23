4918137ee313da9d87112d441f35c148bd1c1b8b
Jack Polak <jpolak@colgate.edu>
2024-03-23 16:11:26 -0400

    user_interface_demo_1

--- dataframe_test.py
+++ dataframe_test.py
@@ -1,13 +1,28 @@
 import pandas as pd
 
-people = 170 #test number, 'people' will show current count
+people = 100 #test number, 'people' will show current count
 time = '7:20 AM' #test time, will show current time
 
 data = {
-  "Frank Dining Hall": [80, 130, people],
+  "Frank Dining Hall": [people],
 }
 
 #load data into a DataFrame object:
-df = pd.DataFrame(data, index = ["6 AM (avg)", "7 AM (avg)", f"{time} (current)"])
+df = pd.DataFrame(data, index = [f"{time}   "])
+
+print(df) 
+
+print()
+
+#placeholder avgs, will update based on data from day
+avg6am = 30
+avg7am = 70 
+
+data = {
+  "Frank Dining Hall": [avg6am, avg7am],
+}
+
+#load data into a DataFrame object:
+df = pd.DataFrame(data, index = ["6 AM (avg)", "7 AM (avg)"])
 
 print(df) 
